8936ff70088786a63ab2d4cdca2a46a0eb96888a
Lucas Livchits <livchitsl@gmail.com>
2019-10-21 23:38:00 -0300

    Sort memes by id

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function filterBySize(meme) {
   return meme.height >= 500 || meme.width >= 500;
 }
 
+function sortById(firstMeme, nextMeme) {
+  return parseInt(firstMeme.id) - parseInt(nextMeme.id)
+}
+
 fetch('https://api.imgflip.com/get_memes')
   .then(response => {
     response.status === 200
@@ -17,4 +21,5 @@ fetch('https://api.imgflip.com/get_memes')
   .then(data => data.json())
   .then(json => json.data.memes.map(meme => memeRestructuring(meme)))
   .then(memes => memes.filter(meme => filterBySize(meme)))
+  .then(memes => memes.sort(sortById)
   .catch(error => console.error(error.message));
