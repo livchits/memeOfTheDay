e5d2c24ae432db147165374e7ef22720fdd1b535
Lucas Livchits <livchitsl@gmail.com>
2019-10-21 23:19:14 -0300

    Extract memes data with map

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function memeRestructuring(meme) {
+  const { id, name, width, height, url } = meme;
+  return { id, name, width, height, url };
+}
+
 fetch('https://api.imgflip.com/get_memes')
   .then(response => {
     response.status === 200
@@ -6,4 +11,5 @@ fetch('https://api.imgflip.com/get_memes')
     return response;
   })
   .then(data => data.json())
+  .then(json => json.data.memes.map(meme => memeRestructuring(meme)))
   .catch(error => console.error(error.message));
