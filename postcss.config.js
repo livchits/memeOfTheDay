b973ca5f735b997fd72d9c1494721ebfc44a08ad
Lucas Livchits <livchitsl@gmail.com>
2019-11-15 21:12:18 -0300

    Format postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano")({
-  preset: "default"
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano')({
+  preset: 'default'
 });
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./dist/index.html"],
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./dist/index.html'],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 });
 
 module.exports = {
-  plugins: [tailwindcss, autoprefixer, cssnano, ...(process.env.NODE_ENV === "production" ? [purgecss] : [])]
+  plugins: [tailwindcss, autoprefixer, cssnano, ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])]
 };
