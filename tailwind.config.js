11395c261a1a45c860442a9f8ada29637c32c5a8
Lucas Livchits <livchitsl@gmail.com>
2019-11-18 17:38:52 -0300

    Add max-heights

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
       bungee: ['Bungee Shade', 'sans-serif'],
       montserrat: ['Montserrat', 'sans-serif'],
       arialB: ['Arial black', 'sans-serif']
+    },
+    maxHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/5': '60%',
+      '3/4': '75%',
+      '60vh': '60vh',
+      full: '100%'
     }
   },
   variants: {
